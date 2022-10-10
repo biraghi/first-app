5ad4b40927418c72f6aba48ed6f6db97d3027cc5
SI-W-00572\Si2001 <lorenzo.biraghi@si2001.it>
2022-10-10 15:23:11 +0200

    Dashboard

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -14,16 +14,12 @@ export class HeroesComponent implements OnInit {
 
   selectedHero?: Hero;
 
-  OnSelect(hero : Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add('HeroesComponent: Selected hero id=${hero.id}')
-  }
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
-
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
