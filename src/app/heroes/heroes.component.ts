76d3f6aa4a8fe0fc814aa637a68f2f72e9af73b3
SI-W-00572\Si2001 <lorenzo.biraghi@si2001.it>
2022-10-18 11:41:02 +0200

    set

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,35 +7,38 @@ import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
 
   selectedHero?: Hero;
 
-
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
 
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 
-  add(name:string): void{
+  add(name: string): void {
     name = name.trim();
-    if(!name) {
+    if (!name) {
       return;
     }
-    this.heroService.addHero({name} as Hero).subscribe(hero=>{this.heroes.push(hero)})
+    this.heroService.addHero({ name } as Hero).subscribe((hero) => {
+      this.heroes.push(hero);
+    });
   }
 
-  delete(hero:Hero): void {
-    this.heroes = this.heroes.filter(h=> h !== hero);
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter((h) => h !== hero);
     this.heroService.deleteHero(hero.id).subscribe();
   }
 
   ngOnInit(): void {
     this.getHeroes();
   }
-
 }
